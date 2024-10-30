da9f28373349d900b06581065f0f11bbd7458410
udcode <yudinoiman@gmail.com>
2024-10-30 14:59:15 -0400

    rest api

--- lecture_8/lib/main.dart
+++ lecture_8/lib/main.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     CancelToken cancelToken = CancelToken();
-   usersApi.getUsers();
+
    todosApi.getTodos();
 usersApi.getUser('sdkjsdk');
     super.initState();
